c0afbfb792779d1cf449acdd33f7b0dda8fc1d4a
dmitry tkachenko <dmitry.tkachenko@rokolabs.com>
2017-12-18 08:02:50 +0400

    Add Content-Type to response

--- SimpleService.WebApi/Helpers/DefaultActionFilter.cs
+++ SimpleService.WebApi/Helpers/DefaultActionFilter.cs
@@ -1,4 +1,5 @@
-﻿using SimpleService.WebApi.Controllers;
+﻿using System;
+using SimpleService.WebApi.Controllers;
 using System.Net.Http.Headers;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
@@ -7,8 +8,39 @@ namespace SimpleService.WebApi
 {
 	public class DefaultActionFilter : ActionFilterAttribute
 	{
+		public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+		{
+			base.OnActionExecuted(actionExecutedContext);
+
+			ContentType contentType = ContentType.None;
+
+			if (actionExecutedContext.ActionContext.ControllerContext.Controller is BaseController baseController)
+			{
+				contentType = baseController.ContentType;
+			}
+
+			string headerContentType;
+
+			switch (contentType)
+			{
+				case ContentType.None:
+				case ContentType.Json:
+					headerContentType = "application/json";
+					break;
+				case ContentType.Xml:
+					headerContentType = "application/xml";
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+
+			actionExecutedContext.Response.Content.Headers.ContentType = new MediaTypeHeaderValue(headerContentType);
+		}
+
 		public override void OnActionExecuting(HttpActionContext actionContext)
 		{
+			base.OnActionExecuting(actionContext);
+
 			ContentType contentType = DefaultActionFilter.GetContentType(actionContext);
 
 			if (actionContext.ControllerContext.Controller is BaseController baseController)
